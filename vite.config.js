3fb83d5ea2e945a5c1b4558aa140b8c7252c8174
SaurabhKumar171 <saurabhkumarjha416@gmail.com>
2023-04-21 11:33:23 +0530

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     server:{
-        host:'https://saurabhkumar171.github.io/',
+        base:'/razorpay-clone/',
     }
     
 })
